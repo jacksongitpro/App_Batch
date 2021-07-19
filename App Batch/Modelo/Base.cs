e992cda05bee9fb93033bf01c653d1e4e027d227
Jackson Garcia <jacksongarcia101@gmail.com>
2021-07-19 17:03:09 -0300

    Add project files.

---
+++ App Batch/Modelo/Base.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Batch.Modelo
+{
+    public static class Base
+    {
+        public static void GenerarReporte()
+        {
+            string Path = @"C:\Users\jacksong\Documents\ProgramacionLogica\Reporte.txt";
+            string[] Lineas;
+            if (Path == null)
+            {
+                Console.WriteLine("Falta Parametro de archivo");
+                return;
+            }
+
+            if (!File.Exists(Path))
+            {
+                Console.WriteLine("archivo No existe");
+                return;
+            }
+            Lineas = File.ReadAllLines(Path);
+
+            List<Material> Materiales = new List<Material>();
+
+            foreach (string registro in Lineas)
+            {
+                string[] campos = registro.Split(';');
+                if(campos.Length !=2)
+                {
+                    throw new Exception("Cantidad de campos invalida");
+                }
+
+                Material M = new Material()
+                {
+                    Nombre = campos[0],
+                    Cantidad = int.Parse(campos [1])
+
+                };Materiales.Add(M);
+            }
+            //ahora creamos un procedimiento que lea la lista creada y defina cual es el mayor y menor 
+
+
+        }
+    }
+}
---
+++ App Batch/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App_Batch.Modelo;
+
+namespace App_Batch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Base.GenerarReporte();
+            
+          
+        }
+    }
+}
